3afab7af83a52a596bf666b650d515c6c6ee32e7
Av-Dev3 <avdevclient@gmail.com>
2025-08-25 17:00:39 -0700

    Add initial setup for Next.js project with essential configuration files, including .gitignore, package-lock.json, and various build and manifest files. Introduce server-side rendering capabilities and prepare for API routes in the admin panel.

--- pages/api/admin/list/media.js
+++ pages/api/admin/list/media.js
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from "../../../../../lib/supabaseServer";
+import { supabaseAdmin } from "../../../../lib/supabaseServer";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).end();
